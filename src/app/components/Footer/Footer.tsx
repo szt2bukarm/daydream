ac312d484949d41f308e8082c561278f5f9aef09
Armin <admin@Armins-MacBook-Pro.local>
2025-05-21 00:49:31 +0200

    m

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,13 +3,13 @@ import FooterWheel from "./FooterWheel";
 
 export default function Footer() {
     return (
-        <div style={{overflow: "hidden"}}>
+        <footer style={{overflow: "hidden"}}>
             <div style={{position: "fixed",width: "100%", height: "720px", bottom: "0", left: 0}}>
                 <FooterNav />
             </div>
             <div style={{marginBottom: "690px"}}>
             <FooterWheel />
             </div>
-        </div>
+        </footer>
     )
 }
